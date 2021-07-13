a5f32c683335423ce6ececf8ce1fe28d9da5e232
ethanl267 <ethanlesch8@gmail.com>
2021-07-13 10:06:19 -0400

    push

--- accounts/models.py
+++ accounts/models.py
@@ -7,3 +7,32 @@ class Customer(models.Model):
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.CharField(max_length=200, null=True)
+
+class Order(models.Model):
+    STATUS = (
+        ('pending', 'pending'),
+        ('Out of delivery', 'Out of delivery'),
+        ('Delivered', 'Delivered'),
+    )
+
+    #customer = 
+    #products = 
+    date_created = models.CharField(max_length=200, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+
